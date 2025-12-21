f6e3629910b6d85dd2c8669bb5098ce42ecd9410
TomVietCode <mcuong04.work@gmail.com>
2025-12-21 22:15:05 +0700

    update client dependencies, add Tailwind CSS theming, and remove unused files

---
+++ client/components/layout/Footer.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+
+export default function Footer() {
+  return (
+    <footer className="border-t py-6 md:py-0">
+      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+            Built by{" "}
+            <a
+              href="#"
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline underline-offset-4"
+            >
+              Aetd. {"   "}
+            </a>
+              All rights reserved.
+          </p>
+        </div>
+        
+        {/* Link phụ ở footer */}
+        <div className="flex gap-4 text-sm text-muted-foreground">
+          Hỗ trợ: <a href="mailto:hanflorist.shop@gmail.com">hanflorist.shop@gmail.com</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
---
+++ client/components/layout/Header.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center p-4">
+        <div className="mr-4 flex">
+          <Link href="/" className="mr-6 flex items-center space-x-2 font-bold">
+            <Image src="/logo.jpg" alt="Logo" width={100} height={100} />
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-end space-x-2">
+          <nav className="hidden not-first:justify-end md:flex items-center space-x-6 text-sm font-medium">
+            <Link href="/chats" className="transition-colors hover:text-foreground/80 text-foreground/60">
+              Chat
+            </Link>
+            <Link href="/posts" className="transition-colors hover:text-foreground/80 text-foreground/60">
+              Bài đăng
+            </Link>
+            <Link href="/notifications" className="transition-colors hover:text-foreground/80 text-foreground/60">
+              Thông báo
+            </Link>
+            <Link href="/" className="transition-colors hover:text-foreground/80 text-foreground/60">
+              Trang chủ
+            </Link>
+          </nav>
+          <Button variant="ghost">Đăng nhập</Button>
+          <Button>Đăng ký</Button>
+        </div>
+      </div>
+    </header>
+  )
+}
